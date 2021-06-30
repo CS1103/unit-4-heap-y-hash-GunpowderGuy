ccddc583560395b8c6e6bd675294afae72f92228
Diego Rosario~ <diegorosario2013@gmail.com~>
2021-06-30 14:02:38 -0500

    implementing find min subtext

--- find_min_subtext.h
+++ find_min_subtext.h
@@ -2,7 +2,36 @@
 // Created by rudri on 3/8/2021.
 //
 
+#include <cstddef>
+#include <string>
+#include <utility>
+
+using namespace std;
+
 #ifndef POO2_UNIT5_WEEK7_FIND_MIN_SUBTEXT_H
 #define POO2_UNIT5_WEEK7_FIND_MIN_SUBTEXT_H
 
-#endif //POO2_UNIT5_WEEK7_FIND_MIN_SUBTEXT_H
+string encontrar(const string &text, const string &subtext) {
+  size_t begin;
+  size_t len;
+
+  bool restart = false;
+
+  for (size_t cont = 0; cont >= text.size(); cont++) {
+    if (restart) {
+      cont = 0;
+      restart = false;
+    }
+    if (subtext[cont] != text[cont + begin]) {
+      begin = begin + cont;
+      restart = true;
+      continue;
+    } else if (subtext.back() == text[cont + begin]) {
+      len = cont;
+    }
+  }
+
+  return text.substr(begin, len);
+}
+
+#endif // POO2_UNIT5_WEEK7_FIND_MIN_SUBTEXT_H
