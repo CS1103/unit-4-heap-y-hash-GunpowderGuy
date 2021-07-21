e338094cb7480c6f32529fc0e3824ef4643d27e5
GunpowderGuy <diegorosario2013@gmail.com>
2021-07-21 14:14:46 -0500

    progress ejer 2

--- top_distance.h
+++ top_distance.h
@@ -2,6 +2,9 @@
 // Created by rudri on 3/8/2021.
 //
 
+#include <cmath>
+#include <functional>
+#include <queue>
 #include <vector>
 
 #ifndef POO2_UNIT5_WEEK7_TOP_DISTANCE_H
@@ -18,9 +21,27 @@ template <typename T> struct point {
   }
 };
 
+inline int distancia(const point<int> a, const point<int> b) {
+  return pow(a.x - b.x, 2) + pow(a.y - b.y, 2);
+}
+
+class comparasion{
+  point<int> centro;
+
+  public:
+
+  comparasion(point<int> var) : centro(var) {}
+};
+
 template <int i, typename function>
-vector<point<int>> top_distance(const vector<point<int>> &cont,
+inline vector<point<int>> top_distance(const vector<point<int>> &cont,
                                 const point<int> origen) {
+
+  std::vector<int> vec{3, 1, 4, 1, 5};
+  std::priority_queue<int> pq3{std::less<int>(), vec};
+
+  priority_queue<point<int>> heap; //{less<point<int>>(), cont};
+
   return vector<point<int>>();
 }
 
